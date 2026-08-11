7f08d7f382159708c172b40d69856c3ef67efe5c
enzopetta77-hash <enzopetta67@gmail.com>
2026-08-11 08:30:15 -0300

    Create media ar.c

---
+++ media ar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main() {
+
+float n1, n2, n3, media;
+
+printf("digite o primeiro valor");
+scanf("%f", &n1);
+
+printf("digite osegundo valor");
+scanf("%f", &n2);
+
+printf("digite o terceiro valor");
+scanf("%f, &n3");
+
+        media= (n1+n2+n3) /3;
+
+printf("a media aritmetica e: %.2f", media);
+return 0;
+}
